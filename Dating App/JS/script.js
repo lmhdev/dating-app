dc5800e2b9ca29e0ed6a735af74762f7805f72c0
lmhdev <84726648+lmhdev@users.noreply.github.com>
2021-07-26 00:37:18 +0700

    Add files via upload

--- Dating App/JS/script.js
+++ Dating App/JS/script.js
@@ -1,6 +1,6 @@
 // Wrap every letter in a span
 // var textWrapper = document.querySelector('.ml9 .letters');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+//   textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 // anime.timeline({loop: true})
 //   .add({
@@ -20,75 +20,75 @@
   
 
   
-  //NAVIGATION
+  //LANDING PAGE NAVIGATION
 
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  if (window.matchMedia('screen and (max-width: 768px)').matches){
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//   if (window.matchMedia('screen and (max-width: 768px)').matches){
 
-  }else{
-    document.getElementById("navbar").style.top = "0";
-    document.getElementById("nav").style.display = "none";
-  }} else {
-  if (window.matchMedia('screen and (max-width: 768px)').matches){
+//   }else{
+//     document.getElementById("navbar").style.top = "0";
+//     document.getElementById("nav").style.display = "none";
+//   }} else {
+//   if (window.matchMedia('screen and (max-width: 768px)').matches){
 
-  }else{
-    document.getElementById("navbar").style.top = "-85px";
-    document.getElementById("nav").style.display = "flex";
-    }
-  }
-}
+//   }else{
+//     document.getElementById("navbar").style.top = "-85px";
+//     document.getElementById("nav").style.display = "flex";
+//     }
+//   }
+// }
 
-var count=0;
-function opennav(){
-  if(count==0){
-    document.getElementById("nav").style.height="200px";
-    document.getElementById("icon").style.color="#2D00F7";
-    document.querySelector(".hamburger").style.display = "flex";
-    count++;
-  }else{
-    document.getElementById("nav").style.height="79px";
-    document.getElementById("icon").style.color="#FFF";
-    document.querySelector(".hamburger").style.display = "none";
-    count=0;
-  }
-}
+// var count=0;
+// function opennav(){
+//   if(count==0){
+//     document.getElementById("nav").style.height="200px";
+//     document.getElementById("icon").style.color="#2D00F7";
+//     document.querySelector(".hamburger").style.display = "flex";
+//     count++;
+//   }else{
+//     document.getElementById("nav").style.height="79px";
+//     document.getElementById("icon").style.color="#FFF";
+//     document.querySelector(".hamburger").style.display = "none";
+//     count=0;
+//   }
+// }
 
-window.addEventListener('resize', closenav);
-function closenav(){
-  if (window.matchMedia('screen and (min-width: 720px)').matches){
-    document.querySelector(".hamburger").style.display = "none";
-    document.getElementById("nav").style.height="79px";
-    count=0;
-  }
-}
+// window.addEventListener('resize', closenav);
+// function closenav(){
+//   if (window.matchMedia('screen and (min-width: 720px)').matches){
+//     document.querySelector(".hamburger").style.display = "none";
+//     document.getElementById("nav").style.height="79px";
+//     count=0;
+//   }
+// }
 
-function openNavBar() {
-  if(count==0){
-    document.getElementById("navbar").style.height="200px";
-    document.getElementById("icon-navbar").style.color="#2D00F7";
-    document.querySelectorAll(".hamburger")[1].style.display = "flex";
-    count++;
-  }else{
-    document.getElementById("navbar").style.height="75px";
-    document.getElementById("icon-navbar").style.color="#FFF";
-    document.querySelectorAll(".hamburger")[1].style.display = "none";
-    count=0;
-  }
-}
+// function openNavBar() {
+//   if(count==0){
+//     document.getElementById("navbar").style.height="200px";
+//     document.getElementById("icon-navbar").style.color="#2D00F7";
+//     document.querySelectorAll(".hamburger")[1].style.display = "flex";
+//     count++;
+//   }else{
+//     document.getElementById("navbar").style.height="75px";
+//     document.getElementById("icon-navbar").style.color="#FFF";
+//     document.querySelectorAll(".hamburger")[1].style.display = "none";
+//     count=0;
+//   }
+// }
 
-window.addEventListener('resize', closeNavBar);
-function closeNavBar(){
-  if (window.matchMedia('screen and (min-width: 720px)').matches){
-    document.querySelectorAll(".hamburger")[1].style.display = "none";
-    document.getElementById("navbar").style.height="75px";
-    count=0;
-  }
-}
+// window.addEventListener('resize', closeNavBar);
+// function closeNavBar(){
+//   if (window.matchMedia('screen and (min-width: 720px)').matches){
+//     document.querySelectorAll(".hamburger")[1].style.display = "none";
+//     document.getElementById("navbar").style.height="75px";
+//     count=0;
+//   }
+// }
 
 
-////HOME PAGE////
+////HOME PAGE NAVBAR////
 
 let btnHomeBurger = document.querySelector("#nav-home-hamburger");
 let navBarHome = document.querySelector(".nav-home");
@@ -100,3 +100,103 @@ btnHomeBurger.onclick = function() {
 btnSearchHome.onclick = function() {
   navBarHome.classList.toggle("active");
 };
+
+
+////HOME PAGE SWIPING AREA////
+
+
+let swipeAreaContainer = document.querySelector('.swipe-area-container');
+let allCards = document.querySelectorAll('.swipe-card');
+let nope = document.getElementById('nope');
+let love = document.getElementById('love');
+
+function initCards(card, index) {
+  let newCards = document.querySelectorAll('.swipe-card:not(.removed)');
+
+  newCards.forEach(function (card, index) {
+    card.style.zIndex = allCards.length - index;
+    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+    card.style.opacity = (10 - index) / 10;
+  });
+  
+  swipeAreaContainer.classList.add('loaded');
+}
+
+initCards();
+
+allCards.forEach(function (el) {
+  let hammertime = new Hammer(el);
+
+  hammertime.on('pan', function (event) {
+    el.classList.add('moving');
+  });
+
+  hammertime.on('pan', function (event) {
+    if (event.deltaX === 0) return;
+    if (event.center.x === 0 && event.center.y === 0) return;
+
+    swipeAreaContainer.classList.toggle('swipe_love', event.deltaX > 0);
+    swipeAreaContainer.classList.toggle('swipe_nope', event.deltaX < 0);
+
+    let xMulti = event.deltaX * 0.03;
+    let yMulti = event.deltaY / 80;
+    let rotate = xMulti * yMulti;
+
+    event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
+  });
+
+  hammertime.on('panend', function (event) {
+    el.classList.remove('moving');
+    swipeAreaContainer.classList.remove('swipe_love');
+    swipeAreaContainer.classList.remove('swipe_nope');
+
+    let moveOutWidth = document.body.clientWidth;
+    let keep = Math.abs(event.deltaX) < 80 || Math.abs(event.velocityX) < 0.5;
+
+    event.target.classList.toggle('removed', !keep);
+
+    if (keep) {
+      event.target.style.transform = '';
+    } else {
+      let endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
+      let toX = event.deltaX > 0 ? endX : -endX;
+      let endY = Math.abs(event.velocityY) * moveOutWidth;
+      let toY = event.deltaY > 0 ? endY : -endY;
+      let xMulti = event.deltaX * 0.03;
+      let yMulti = event.deltaY / 80;
+      let rotate = xMulti * yMulti;
+
+      event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+      initCards();
+    }
+  });
+});
+
+function createButtonListener(love) {
+  return function (event) {
+    let cards = document.querySelectorAll('.swipe-card:not(.removed)');
+    let moveOutWidth = document.body.clientWidth * 1.5;
+
+    if (!cards.length) return false;
+
+    let card = cards[0];
+
+    card.classList.add('removed');
+
+    if (love) {
+      card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+    } else {
+      card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+    }
+
+    initCards();
+
+    event.preventDefault();
+  };
+}
+
+let nopeListener = createButtonListener(false);
+let loveListener = createButtonListener(true);
+
+nope.addEventListener('click', nopeListener);
+love.addEventListener('click', loveListener);
